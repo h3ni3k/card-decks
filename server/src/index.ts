53a9639dc2648ddaeaa87e3e9a6a40dec2c00364
h3ni3k <h3niek01@gmail.com>
2022-11-27 17:57:12 +0100

    working on API, added  post and get /deck endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,30 @@
-import express from 'express';
-const app = express();
+import { config } from 'dotenv';
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+config();
 
-app.listen(5000);
+import Deck from './models/Deck';
+
+const PORT = 5000;
+const app = express();
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from the base route');
+});
+
+app.get('/decks', (req: Request, res: Response) => {
+  res.send('Decks');
+});
+
+app.post('/decks', async (req: Request, res: Response) => {
+  const newDeck = new Deck({ title: req.body.title });
+
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+});
+
+mongoose.connect(process.env.MONGO_URL ?? '').then(() => {
+  console.log(`App listening on port ${PORT}`);
+  app.listen(PORT);
+});
